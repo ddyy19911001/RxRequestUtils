adbf21e09ec3c4c96f8f8b96cbb0e474662d65fe
dengyin <584120792@qq.com>
2020-08-05 17:07:24 +0800

    修改请求结果打印方法，解决请求结果的打印问题

--- xsnow/src/main/java/com/vise/xsnow/http/request/BaseHttpRequest.java
+++ xsnow/src/main/java/com/vise/xsnow/http/request/BaseHttpRequest.java
@@ -87,7 +87,7 @@ public abstract class BaseHttpRequest<R extends BaseHttpRequest> extends BaseReq
         logTag=httpGlobalConfig.getTag();
         String url = (this.baseUrl == null ? httpGlobalConfig.getBaseUrl() : this.baseUrl) + suffixUrl;
         String requestBodyLog="\n请求地址："+url+"\n"+"请求时间："+format.format(new Date())+"\n请求头："+headerStr+"\n请求参数："+paramsStr;
-        Log.i(logTag, requestBodyLog);
+        Log.i(logTag+"\n", requestBodyLog);
         execute(callback);
     }
 
